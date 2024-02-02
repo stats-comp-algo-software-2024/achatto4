4127c1920e4d157e96340870ab871dcec1ae5858
Anagh Chattopadhyay <anagh72@gmail.com>
2024-02-02 16:52:27 -0500

    Final changes to skeleton

--- R/hyper_logit.R
+++ R/hyper_logit.R
@@ -7,8 +7,6 @@
 #' @return An object of class 'hiper_glm'.
 #' @export
 hiper_glm <- function(design, outcome) {
-  # Your implementation of high-performance GLM here
-  # For example, using glm function for simplicity
   model <- glm(outcome ~ design, family = "binomial")
   class(model) <- "hiper_glm"
   return(model)
--- R/model_output.R
+++ R/model_output.R
@@ -1,20 +1,17 @@
 #' @export
 coef.hiper_glm <- function(object, ...) {
-  # Your implementation of coef method
-  # For example, returning coefficients from the object
+  # Returning coefficients from the object(can be changed accordingly)
   return(coef(object))
 }
 
 #' @export
 vcov.hiper_glm <- function(object, ...) {
-  # Your implementation of vcov method
-  # For example, returning the variance-covariance matrix from the object
+  # Returning var-cov matrix from the object(can be changed accordingly)
   return(vcov(object))
 }
 
 #' @export
 print.hiper_glm <- function(x, ...) {
-  # Your implementation of print method
   # For example, printing relevant information about the model
   cat("High-Performance GLM Model\n")
   cat("Call: ", deparse(x$call), "\n")
